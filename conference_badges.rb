ba7fb412e17dd55fd0353e65b83b522ff013c3cb
Adrian Samaniego <llathander@gmail.com>
2019-05-17 22:13:14 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -3,5 +3,25 @@
 # conference badges
 
 @Names = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz" ]
-def badge_maker (x)
-  for (x) = 
+def badge_maker(name)
+  return "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(list_ofspeakers)
+  list_ofspeakers.collect do |speaker|
+    "Hello, my name is #{speaker}"
+  end
+end
+
+def assign_rooms(speakers)
+  speakers.collect.each_with_index {|speaker, room|
+    "Hello, #{speaker}! You'll be assigned to #{room+1}!"}
+end
+
+def printer(attendees)
+  assign_rooms(attendees).each do |assignment|
+    puts assignment
+  end
+end
+  
+printer(["Lindsay","Rachel","molly"])
